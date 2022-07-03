83671c36fa4c780d91d658321c15b986b1003c2e
Tabitha Wallace <tmwallace20@outlook.com>
2022-07-03 16:05:13 -0400

    Update in progress, made markers circle

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -43,9 +43,12 @@ let cityData = cities;
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
     console.log(city)
-    L.marker(city.location)
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
-  .addTo(map);
+    L.circleMarker(city.location, {
+      radius: city.population/200000
+      fillColor: 'red',
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
 });
 
 // We create the tile layer that will be the background of our map.
