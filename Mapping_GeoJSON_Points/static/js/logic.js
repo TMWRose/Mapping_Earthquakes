bc7a554bcedd97b93ed2e58153eb4bb59919a1d1
Tabitha Wallace <tmwallace20@outlook.com>
2022-07-05 15:14:45 -0400

    updating for trouble shoot

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,82 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
-
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-
-
-// Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport, {
-    onEachFeature: function(feature, layer) {
-      console.log(layer);
-      layer.bindPopup();
-    }
-  }).addTo(map);
-
-// An array containing each city's location, state, and population.
-let cities = [{
-  location: [40.7128, -74.0059],
-  city: "New York City",
-  state: "NY",
-  population: 8398748
-},
-{
-  location: [41.8781, -87.6298],
-  city: "Chicago",
-  state: "IL",
-  population: 2705994
-},
-{
-  location: [29.7604, -95.3698],
-  city: "Houston",
-  state: "TX",
-  population: 2325502
-},
-{
-  location: [34.0522, -118.2437],
-  city: "Los Angeles",
-  state: "CA",
-  population: 3990456
-},
-{
-  location: [33.4484, -112.0740],
-  city: "Phoenix",
-  state: "AZ",
-  population: 1660272
-}
-];
-
-// Get data from cities.js
-let cityData = cities;
-
-// Loop through the cities array and create one marker for each city.
-//cityData.forEach(function(city) {
-  //  console.log(city)
-  //  L.circleMarker(city.location, {
-   //   radius: city.population/200000
-   // })
-   // .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-   // .addTo(map);
-//});
+let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -88,6 +13,91 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+    // Creating a GeoJSON layer with the reterieved data.
+    L.geoJson(data).addTo(map);
+});
+
+// // Add GeoJSON data.
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
+
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+    onEachFeature: function(feature, layer) {
+      console.log(layer);
+      layer.bindPopup();
+    }
+  }).addTo(map);
+
+// // An array containing each city's location, state, and population.
+// let cities = [{
+//   location: [40.7128, -74.0059],
+//   city: "New York City",
+//   state: "NY",
+//   population: 8398748
+// },
+// {
+//   location: [41.8781, -87.6298],
+//   city: "Chicago",
+//   state: "IL",
+//   population: 2705994
+// },
+// {
+//   location: [29.7604, -95.3698],
+//   city: "Houston",
+//   state: "TX",
+//   population: 2325502
+// },
+// {
+//   location: [34.0522, -118.2437],
+//   city: "Los Angeles",
+//   state: "CA",
+//   population: 3990456
+// },
+// {
+//   location: [33.4484, -112.0740],
+//   city: "Phoenix",
+//   state: "AZ",
+//   population: 1660272
+// }
+// ];
+
+// // Get data from cities.js
+// let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+//cityData.forEach(function(city) {
+  //  console.log(city)
+  //  L.circleMarker(city.location, {
+   //   radius: city.population/200000
+   // })
+   // .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+   // .addTo(map);
+//});
 
 
 
