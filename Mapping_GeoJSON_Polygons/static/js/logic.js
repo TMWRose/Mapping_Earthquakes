b0d53c3d40dc0a66616f51d89aac27d3330586a4
Tabitha Wallace <tmwallace20@outlook.com>
2022-07-06 12:21:23 -0400

    created new branch

---
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -0,0 +1,135 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Streets": streets,
+  "Satellite Streets": satelliteStreets
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [43.7, -79.3],
+  zoom: 11,
+  layers: [satelliteStreets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map)
+
+// // Accessing the Toronto airline routes GeoJSON URL.
+// let torontoData = "https://raw.githubusercontent.com/TMWRose/Mapping_Earthquakes/Mapping_GeoJSON_Linestring/Mapping_GeoJSON_Linestring/static/js/torontoRoutes.json";
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+// Then we add our 'graymap' tile layer to the map.
+light.addTo(map);
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
+
+// Grabbing our GeoJSON data.
+d3.json(torontoHoods).then(function(data) {
+  console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
+
+
+
+// // Add GeoJSON data.
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
+
+
+// // Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport, {
+//     onEachFeature: function(feature, layer) {
+//       console.log(layer);
+//       layer.bindPopup();
+//     }
+//   }).addTo(map);
+
+// // An array containing each city's location, state, and population.
+// let cities = [{
+//   location: [40.7128, -74.0059],
+//   city: "New York City",
+//   state: "NY",
+//   population: 8398748
+// },
+// {
+//   location: [41.8781, -87.6298],
+//   city: "Chicago",
+//   state: "IL",
+//   population: 2705994
+// },
+// {
+//   location: [29.7604, -95.3698],
+//   city: "Houston",
+//   state: "TX",
+//   population: 2325502
+// },
+// {
+//   location: [34.0522, -118.2437],
+//   city: "Los Angeles",
+//   state: "CA",
+//   population: 3990456
+// },
+// {
+//   location: [33.4484, -112.0740],
+//   city: "Phoenix",
+//   state: "AZ",
+//   population: 1660272
+// }
+// ];
+
+// // Get data from cities.js
+// let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+//cityData.forEach(function(city) {
+  //  console.log(city)
+  //  L.circleMarker(city.location, {
+   //   radius: city.population/200000
+   // })
+   // .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+   // .addTo(map);
+//});
+
+
+
+
